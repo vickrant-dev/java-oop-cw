29f4176fcf011fc4ccd8bc3be11b8de60ef038d0
Rahul Shudarshana <rahulshudarshana@gmail.com>
2025-12-31 00:09:45 +0530

    Designed the CustomerPanel and And the TransactionForm

--- app/src/main/java/com/inventory/main.java
+++ app/src/main/java/com/inventory/main.java
@@ -1,11 +1,22 @@
 package com.inventory;
 
 import com.inventory.ui.auth.login.*;
+import com.inventory.ui.dashboard.customer_management.CustomerPanel;
+
+import javax.swing.*;
 
 public class main {
     public static void main (String[] args) {
-        System.out.println("Main");
+        /*System.out.println("Main");
         Login login = new Login();
-        login.setVisible(true);
+        login.setVisible(true);*/
+
+        JFrame frame = new JFrame("Product Management");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900,500);
+        frame.setLocationRelativeTo(null);
+
+        frame.add(new CustomerPanel());
+        frame.setVisible(true);
     }
 }
